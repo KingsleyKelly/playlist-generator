a3c66e4d7622684225bf0ecadb5e627703b02c0d
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-07 17:15:06 +0100

    lil change

--- app/todo/views.py
+++ app/todo/views.py
@@ -7,7 +7,7 @@ from django.template import defaultfilters
 
 @csrf_protect
 def index(request):
-    tracks = [track.youtube_id for track in Track.objects.all()]
+    tracks = [unicode(track.youtube_id) for track in Track.objects.all()]
 
     return render(request, 'todo.html', {'tracks': tracks})
         
