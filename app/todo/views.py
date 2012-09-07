0844e7dd7c46f6a6e09d222beca805422a4f1b48
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-07 17:20:54 +0100

    lil change

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
-
 from models import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response, render
 from django.template import defaultfilters
+from django.utils import simplejson
 
 @csrf_protect
 def index(request):
-    tracks = [unicode(track.youtube_id) for track in Track.objects.all()]
-
-    return render(request, 'todo.html', {'tracks': tracks})
+    tracks = [(track.youtube_id) for track in Track.objects.all()]
+    json_tracks = simplejson.dumps(tracks)
+    return render(request, 'todo.html', {'tracks': json_tracks})
         
 
         
