e47b2adc4d7030f4af85ade9ca2718e435bdb8b3
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-08 15:45:03 +0100

    change

--- app/todo/models.py
+++ app/todo/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Playlist(models.Model):
   name = models.CharField(max_length=200)
-  
+
 class Track(models.Model):
-  rank = models.IntegerField(max_length=5)
+  rank = models.IntegerField()
   youtube_id = models.CharField(max_length=200)
   artist_name = models.CharField(max_length=200)
   track_name = models.CharField(max_length=200)
