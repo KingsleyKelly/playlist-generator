4cf231a7b8166bf62e5e5b750d7610aff1b4fa02
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-08 16:19:37 +0100

    lil change

--- app/todo/views.py
+++ app/todo/views.py
@@ -10,11 +10,10 @@ from django.utils import simplejson
 def index(request):
     youtube_ids = [track.youtube_id for track in Track.objects.all()]
     full_tracks = Track.objects.all()
-    json_artists_with_tracks = simplejson.dumps(full_tracks) 
     json_youtube_ids = simplejson.dumps(youtube_ids)
     return render(request, 'todo.html', 
                             {'tracks': json_youtube_ids,
-                             'artists_with_tracks': json_artists_with_tracks})
+                             'artists_with_tracks': full_tracks})
 
 
         
