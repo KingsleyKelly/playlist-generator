437d75814ea1ff75fd85e7e6880767d88e36fe40
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-08 14:56:56 +0100

    sharp intake of breath

--- app/todo/models.py
+++ app/todo/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+
+class Playlist(models.Model):
+  name = models.CharField(max_length=200)
 class Track(models.Model):
-    youtube_id = models.CharField(max_length=200)
-    
+  rank = models.IntegerField(max_length=5)
+  youtube_id = models.CharField(max_length=200)
+  artist_name = models.CharField(max_length=200)
+  track_name = models.CharField(max_length=200)
+
--- app/todo/views.py
+++ app/todo/views.py
@@ -8,9 +8,13 @@ from django.utils import simplejson
 
 @csrf_protect
 def index(request):
-    tracks = [track.youtube_id for track in Track.objects.all()]
-    json_tracks = simplejson.dumps(tracks)
-    return render(request, 'todo.html', {'tracks': json_tracks})
+    youtube_ids = [track.youtube_id for track in Track.objects.all()]
+    artists = [track.artist_name for track in Track.objects.all()]
+    track_names = track.track_name for track in Track.objects.all()]
+    artists_with_track_names = zip(artists, track_names)
+    json_artists_with_tracks = simplejson.dumps(artists_with_track_names) 
+    json_youtube_ids = simplejson.dumps(youtube_ids)
+    return render(request, 'todo.html', {'tracks': json_youtube_ids})
         
 
         
