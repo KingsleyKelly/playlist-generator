72a8fdda6d38b6c1641a0a67a224e321793432bc
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-17 14:14:04 +0100

    lil change

--- app/todo/views.py
+++ app/todo/views.py
@@ -17,14 +17,14 @@ def index(request):
                               {'tracks': json_youtube_ids,
                                'artists_with_tracks': tracks})
     else:
-      try:
-        playlist = Playlist.objects.get(name=request.POST['data'])
-        tracks = playlist.track_set.all()
-        youtube_ids = [track.youtube_id for track in tracks]
-        json_youtube_ids = simplejson.dumps(youtube_ids)
-        return HttpResponse({'tracks': json_youtube_ids,
-                             'artists_with_tracks': tracks}, 
-                              mimetype="application/javascript")      
+      
+      playlist = Playlist.objects.get(name=request.POST['data'])
+      tracks = playlist.track_set.all()
+      youtube_ids = [track.youtube_id for track in tracks]
+      json_youtube_ids = simplejson.dumps(youtube_ids)
+      return HttpResponse({'tracks': json_youtube_ids,
+                           'artists_with_tracks': tracks}, 
+                            mimetype="application/javascript")      
 
 
         
