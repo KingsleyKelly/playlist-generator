cc7366ab646df1114b9a5f5daed3e48546126ff7
KingsleyKelly <kingsley_kelly@gmail.com>
2012-09-23 12:29:53 +0100

    lil change

--- app/settings.py
+++ app/settings.py
@@ -77,7 +77,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-   'django.contrib.staticfiles.finders.DefaultStorageFinder',
+   # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -110,7 +110,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'todo',
-    'dajaxice',
+    # 'dajaxice',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
--- app/todo/views.py
+++ app/todo/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response, render
 from django.template import defaultfilters
 from django.utils import simplejson
+from django.views.generic.detail import BaseDetailView, \
+    SingleObjectTemplateResponseMixins
 
 @csrf_protect
 def index(request):
@@ -24,9 +26,26 @@ def index(request):
       json_youtube_ids = simplejson.dumps(youtube_ids)
       return HttpResponse({'tracks': json_youtube_ids,
                            'artists_with_tracks': tracks}, 
-                            mimetype="application/javascript")      
+                            mimetype="application/javascript")  
+
+# class JSONResponseMixin(object):
+#   def render_to_response(self, context):
+#     return self.get_json_response(self.convert_contect_to_json(context))
+#   def get_json_response(self, contennt, **httprespone_kwargs):
+#     return HttpResonse(content, content_type='application/json', **httpresponse)
+#   def convert_context_to_json(self, context):
+#     return simplejson.dumps(context)
+
+# class HybridDetailView(JSONResponseMixin, SingleObjectTemplateResponseMixin, BaseDetailView)
+#   def render_to_response(self, context):
+#     if self.request.is_ajax():
+#       obj = context['object'].as_dict()
+#       return JSONResponseMixin.render_to_response(self,obj)
+#     else:
+#       return SingleObjectTemplateResponseMixin.render_to_response(self, context)
+
 
 
         
 
-        
+#         
